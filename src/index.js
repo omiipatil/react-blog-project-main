91b01fd67e9a6faf17adca9700c5a0e2750bfb11
omiipatil <92794706+omiipatil@users.noreply.github.com>
2022-03-21 23:12:07 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import {Provider} from "react-redux";
+// import store from "./Redux/store.js";
+// // import App1 from './App1.js';
+// import {App2} from "./App2.js";
+
+
+ReactDOM.render(
+  
+  
+    <App />,
+  
+
+  document.querySelector("#root")
+);
+
